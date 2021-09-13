4de3801d6a2fdaf5f8fd8f443f74ff9f34d285d7
oushihao <oushihao97@163.com>
2021-09-13 17:22:09 +0800

    test(two-sum): 实现暴力解法

--- src/two-sum/index.spec.ts
+++ src/two-sum/index.spec.ts
@@ -1,15 +1,33 @@
 import {twoSum} from "./index";
 
-describe('matching cities to foods', () => {
-    test('adds 1 + 2 to equal 3', () => {
-        // Arrange
-        let x: number[] = [1, 2], y: number = 2;
-        let expected: number = 2;
-
-        // Act
-        let actual: number = twoSum(x, y);
-
-        // Assert
-        expect(actual).toBe(expected);
+describe('两数之和', () => {
+    describe('暴力解法', () => {
+        testCase(twoSum);
     });
 });
+
+function testCase(fn: Function) {
+    test('示例一', () => {
+        let nums: number[] = [2, 7, 11, 15];
+        let target: number = 9;
+        let expected: number[] = [0, 1];
+
+        expect(fn(nums, target)).toEqual(expected);
+    });
+
+    test('示例二', () => {
+        let nums: number[] = [3, 2, 4];
+        let target: number = 6;
+        let expected: number[] = [1, 2];
+
+        expect(fn(nums, target)).toEqual(expected);
+    });
+
+    test('示例三', () => {
+        let nums: number[] = [3, 3];
+        let target: number = 6;
+        let expected: number[] = [0, 1];
+
+        expect(fn(nums, target)).toEqual(expected);
+    });
+}
--- src/two-sum/index.ts
+++ src/two-sum/index.ts
@@ -1,4 +1,13 @@
-export function twoSum(nums: number[], target: number): number {
-    console.log(nums, target)
-    return target;
+/**
+ * 暴力解法
+ * @param nums
+ * @param target
+ */
+export function twoSum(nums: number[], target: number): number[] {
+    for(let i: number = 0; i < nums.length; i++) {
+        for(let j: number = i + 1; j < nums.length; j++) {
+            if(nums[i] + nums[j] === target) return [i, j];
+        }
+    }
+    return [];
 }
