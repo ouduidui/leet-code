2f22e89bb6d6fa6e20f78b229b288581b7058234
oushihao <oushihao97@163.com>
2021-11-21 11:24:18 +0800

    feat(substring-with-concatenation-of-all-words): 实现滑动窗口解法

--- README.md
+++ README.md
@@ -39,3 +39,4 @@ yarn cli
 | 27 | [移除元素](src/remove-element/README.md) | 简单 | https://leetcode-cn.com/problems/remove-element/ |
 | 28 | [实现strStr()](src/implement-strstr/README.md) | 简单 | https://leetcode-cn.com/problems/implement-strstr/ |
 | 29 | [两数相除](src/divide-two-integers/README.md) | 中等 | https://leetcode-cn.com/problems/divide-two-integers/ |
+| 30 | [串联所有单词的子串](src/substring-with-concatenation-of-all-words/README.md) | 困难 | https://leetcode-cn.com/problems/substring-with-concatenation-of-all-words/ |
---
+++ src/substring-with-concatenation-of-all-words/index.spec.ts
@@ -0,0 +1,33 @@
+import {findSubstring} from "./index"
+
+describe('串联所有单词的子串', () => {
+    test('滑动窗口', () => {
+        testCase(findSubstring)
+    })
+});
+
+function testCase(fn: Function) {
+    test('示例一', () => {
+        const s: string = "barfoothefoobarman";
+        const words: string[] = ["foo", "bar"];
+        const expected: number[] = [0, 9];
+
+        expect(fn(s, words)).toEqual(expected);
+    })
+
+    test('示例二', () => {
+        const s: string = "wordgoodgoodgoodbestword";
+        const words: string[] = ["word","good","best","word"];
+        const expected: number[] = [];
+
+        expect(fn(s, words)).toEqual(expected);
+    })
+
+    test('示例三', () => {
+        const s: string = "barfoofoobarthefoobarman";
+        const words: string[] = ["bar","foo","the"];
+        const expected: number[] = [6,9,12];
+
+        expect(fn(s, words)).toEqual(expected);
+    })
+}
